5fc849da107cee8065d91b2a1dc3db03d636291c
Aldi Gunawan <aldigunawan458@gmail.com>
2022-05-19 22:19:22 +0700

    change api maps

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 const MONGODB_URI = process.env.MONGODB_URI;
 const SECRET_KEY = process.env.SECRET_KEY;
 const STATIC_MAP_API = 'AIzaSyCsDozfpFuwGMc8uRZhFweZhT3WlK3U7UU';
-const MAP_API = 'AIzaSyCzUzIuGgRUQvNz2k1ro-Y6NhkX4INswr0';
+const MAP_API = 'AIzaSyBXxYaDI5-UUkfyNlnZUDTPGYVgPwEO_Rw';
 const PRIVATE_VAPID_KEY = 'yu15zf_MqQE-wrdG-bLzl4gzyBWOaD3V6cepT1-Dd1s';
 const PUBLIC_VAPID_KEY =
   'BJUByaKO6fCdTpIJXPgzAUOuKluvE11vyBn9m9EgsGjahBJ9MQwwasxQtchdBprUXMFeFLB8xEo_k-TBJMWXP84';
