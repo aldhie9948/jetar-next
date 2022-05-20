05f048185934a81c9f37323ed5d3d336466efcc1
Aldi Gunawan <aldigunawan458@gmail.com>
2022-05-20 14:21:27 +0700

    error handler changes

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -2,6 +2,7 @@ import { toast } from '../components/Sweetalert2';
 
 export const axiosError = (error) => {
   // toast({ title: error.response.data.error, icon: 'error' });
-  console.log(error.response.data.error);
+  console.log(error);
+  // console.log(error.response.data.error);
   return { error: error.response.data.error };
 };
