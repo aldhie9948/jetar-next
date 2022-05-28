dee655bf273bbaeb2177f51f1c88638b06036462
Aldi Gunawan <aldigunawan458@gmail.com>
2022-05-28 21:10:27 +0700

    move put method to [id]

--- pages/api/subscription/[id].js
+++ pages/api/subscription/[id].js
@@ -11,19 +11,35 @@ const handler = nc({
   onNoMatch: (req, res) => {
     res.status(404).end('Halaman tidak ditemukan');
   },
-}).get(async (req, res) => {
-  await connect();
-  try {
-    const {
-      query: { id },
-    } = req;
-    verifyToken(req);
-    const pengguna = await Pengguna.findById(id);
-    res.status(200).json({ status: 'OK', data: pengguna });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ status: 'FAILED', data: error });
-  }
-});
+})
+  .get(async (req, res) => {
+    await connect();
+    try {
+      const {
+        query: { id },
+      } = req;
+      verifyToken(req);
+      const pengguna = await Pengguna.findById(id);
+      res.status(200).json({ status: 'OK', data: pengguna });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ status: 'FAILED', data: error });
+    }
+  })
+  .put(async (req, res) => {
+    await connect();
+    try {
+      const {
+        query: { id },
+      } = req;
+      const body = req.body;
+      verifyToken(req);
+      const save = await Pengguna.findByIdAndUpdate(id, body, { new: true });
+      res.status(201).json({ status: 'OK', data: save });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ status: 'FAILED', data: error });
+    }
+  });
 
 export default handler;
--- pages/api/subscription/index.js
+++ pages/api/subscription/index.js
@@ -11,33 +11,16 @@ const handler = nc({
   onNoMatch: (req, res) => {
     res.status(404).end('Halaman tidak ditemukan');
   },
-})
-  .get(async (req, res) => {
-    await connect();
-    try {
-      // verifyToken(req);
-      const pengguna = await Pengguna.find({});
-      res.status(200).json({ status: 'OK', data: pengguna });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ status: 'FAILED', data: error });
-    }
-  })
-  .put(async (req, res) => {
-    await connect();
-    try {
-      const { id, subscription } = req.body;
-      verifyToken(req);
-      const save = await Pengguna.findByIdAndUpdate(
-        id,
-        { subscription },
-        { new: true }
-      );
-      res.status(201).json({ status: 'OK', data: save });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ status: 'FAILED', data: error });
-    }
-  });
+}).get(async (req, res) => {
+  await connect();
+  try {
+    // verifyToken(req);
+    const pengguna = await Pengguna.find({});
+    res.status(200).json({ status: 'OK', data: pengguna });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: 'FAILED', data: error });
+  }
+});
 
 export default handler;
